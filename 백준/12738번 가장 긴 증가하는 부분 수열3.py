b928172ae57bf5027708daf633be2fadbe410034
chanw12 <wooju9812127@gmail.com>
2023-02-09 12:12:40 +0900

    12738번 가장 긴 증가하는 부분수열3

---
+++ 백준/12738번 가장 긴 증가하는 부분 수열3.py
@@ -0,0 +1,19 @@
+n = int(input())
+ll = list(map(int,input().split()))
+temp = [0]
+
+for l in ll:
+    if temp[-1] < l:
+        temp.append(l)
+    else:
+        left = 0
+        right = len(temp)-1
+
+        while left < right:
+            mid = (left+ right )//2
+            if temp[mid] < l:
+                left= mid + 1
+            else:
+                right = mid
+        temp[right] = l
+print(len(temp)-1)
